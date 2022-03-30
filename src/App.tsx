a87b40f69ac47e4f364031d741a5acaefe8c6313
Steve Daniels <steve@fullstacksteve.co.uk>
2022-03-30 11:32:42 +0100

    Refactored buttons into ButtonGroup WIP

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ import './App.css'
 import { createEmotionCache } from './ui/functions'
 import { CacheProvider, ThemeProvider } from '@emotion/react'
 import theme, { GlobalStyles } from './theme'
-import { Container, CssBaseline, Box, Typography, Button, Grid } from '@mui/material'
+import { Container, CssBaseline, Box, Typography } from '@mui/material'
 import { palette } from './theme/constants'
+import { ButtonGroup } from './ui/components'
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache()
@@ -37,41 +38,7 @@ function App() {
                   <Typography variant="h1" component="h1" textAlign="center" gutterBottom>
                      Edit <code>src/App.tsx</code> and save to reload.
                   </Typography>
-                  <Grid container spacing={2} sx={{ maxWidth: 600 }}>
-                     <Grid item xs={12} md={4} mt={5}>
-                        <Box sx={{ ...GlobalStyles.flexBox }}>
-                           <Button
-                              variant="contained"
-                              color="secondary"
-                              onClick={() => alert('Button pressed')}
-                           >
-                              Next
-                           </Button>
-                        </Box>
-                     </Grid>
-                     <Grid item xs={12} md={4} mt={5}>
-                        <Box sx={{ ...GlobalStyles.flexBox }}>
-                           <Button
-                              variant="contained"
-                              color="secondary"
-                              onClick={() => alert('Button pressed')}
-                           >
-                              Save
-                           </Button>
-                        </Box>
-                     </Grid>
-                     <Grid item xs={12} md={4} mt={5}>
-                        <Box sx={{ ...GlobalStyles.flexBox }}>
-                           <Button
-                              variant="contained"
-                              color="secondary"
-                              onClick={() => alert('Button pressed')}
-                           >
-                              Favourites
-                           </Button>
-                        </Box>
-                     </Grid>
-                  </Grid>
+                  <ButtonGroup />
                </Box>
             </Container>
          </ThemeProvider>
