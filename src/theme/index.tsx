abd042b81c48f784599cbe684e6bda19f37e73a7
Steve Daniels <steve@fullstacksteve.co.uk>
2022-03-30 10:42:46 +0100

    Added custom MUI palette

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -2,8 +2,12 @@
 import type {} from '@mui/lab/themeAugmentation'
 
 import { createTheme, responsiveFontSizes } from '@mui/material/styles' // import { TypographyOptions } from '@mui/material/styles/createTypography'
+import { palette } from './constants'
 
-let theme = createTheme({})
+let theme = createTheme({
+   // MUI Palette overrides
+   palette: palette
+})
 
 theme = responsiveFontSizes(theme)
 
