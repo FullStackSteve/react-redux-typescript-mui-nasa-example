b6dfaa53850e6248492b544e912f3216318ce45d
Steve Daniels <steve@fullstacksteve.co.uk>
2022-03-30 11:51:54 +0100

    App now uses Router component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,8 @@
-import React from 'react'
-import logo from './logo.svg'
 import './App.css'
-import { Typography } from '@mui/material'
-import { ButtonGroup } from './ui/components'
-import Layout from './ui/components/Layout'
+import Router from './router'
 
 function App() {
-   return (
-      <Layout>
-         <img src={logo} className="App-logo" alt="logo" />
-         <Typography variant="h1" component="h1" textAlign="center" gutterBottom>
-            Edit <code>src/App.tsx</code> and save to reload.
-         </Typography>
-         <ButtonGroup />
-      </Layout>
-   )
+   return <Router />
 }
 
 export default App
