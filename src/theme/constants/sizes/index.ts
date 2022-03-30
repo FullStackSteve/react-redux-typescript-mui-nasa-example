9fc1b92df2e2444063e84c23244b3d30840d3053
Steve Daniels <steve@fullstacksteve.co.uk>
2022-03-30 10:38:58 +0100

    Added custom sizing for fonts

---
+++ src/theme/constants/sizes/index.ts
@@ -0,0 +1,13 @@
+export const sizes = {
+   largest: 48,
+   larger: 34,
+   large: 30,
+   regular: 24,
+   medium: 20,
+   small: 18,
+   smallest: 14,
+   input: 18,
+   button: 18
+}
+
+export default sizes
