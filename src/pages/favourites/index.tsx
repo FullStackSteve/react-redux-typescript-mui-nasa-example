0c32820edab0493c02da58ea010a62eeae88da00
Steve Daniels <steve@fullstacksteve.co.uk>
2022-03-31 15:00:00 +0100

    Favourite page now uses ImageGrid

--- src/pages/favourites/index.tsx
+++ src/pages/favourites/index.tsx
@@ -1,12 +1,19 @@
-import { Typography } from '@mui/material'
-import { Layout } from 'src/ui/components'
+import { Button, Typography } from '@mui/material'
+import { Link } from 'react-router-dom'
+import useNasaPhoto from 'src/hooks/useNasaPhoto'
+import { ImageGrid, Layout } from 'src/ui/components'
 
 function Favourites() {
+   const { favouritePhotos } = useNasaPhoto()
    return (
       <Layout>
          <Typography variant="h1" component="h1" textAlign="center" gutterBottom>
             Favourites
          </Typography>
+         <ImageGrid images={favouritePhotos} />
+         <Button variant="contained" color="secondary" component={Link} to={'/'}>
+            See more photos
+         </Button>
       </Layout>
    )
 }
--- src/ui/components/index.ts
+++ src/ui/components/index.ts
@@ -1,2 +1,4 @@
 export { default as ButtonGroup } from './ButtonGroup'
 export { default as Layout } from './Layout'
+export { default as ImageGrid } from './ImageGrid'
+export { default as NavBar } from './NavBar'
